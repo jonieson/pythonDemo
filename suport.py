2467c887161d0047305b619988e8427d34e9c5f3
ydz <2439535923@qq.com>
2017-08-10 16:02:45 +0800

    update
    commit about module code

---
+++ suport.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8 -*-
+
+def logString(parama):
+    print '打印方法调用',parama
+
+def userName():
+    print 'userName'
+
---
+++ testModel.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8 -*-
+# 引入自定义functionDemo类
+import suport
+# 引入自定义的suport类中的userNAme方法
+from suport import userName
+
+suport.logString('调用类')
+userName()
